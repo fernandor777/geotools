c7254229d6cc7e72e8abaa2e4ee1dfe461d3e54f
Fernando Miño <fernandomino7@gmail.com>
2018-06-01 12:16:18 -0500

    AppSchema Index XML Configurations load + code format

--- modules/extension/app-schema/app-schema/src/main/java/org/geotools/data/complex/config/AttributeMapping.java
+++ modules/extension/app-schema/app-schema/src/main/java/org/geotools/data/complex/config/AttributeMapping.java
@@ -128,6 +128,9 @@ public class AttributeMapping implements Serializable {
 
     private MultipleValue multipleValue;
 
+    /** Field name in external index layer */
+    private String indexField;
+
     /**
      * Mapping of attributes with 1..N cardinality. Each data store is free to contribute is own
      * syntax (e.g. Solr multi values to support multi valuated fields).
@@ -467,5 +470,13 @@ public class AttributeMapping implements Serializable {
 
     public void setMultipleValue(MultipleValue multipleValue) {
         this.multipleValue = multipleValue;
+    }
+
+    public String getIndexField() {
+        return indexField;
+    }
+
+    public void setIndexField(String indexField) {
+        this.indexField = indexField;
     }
 }
