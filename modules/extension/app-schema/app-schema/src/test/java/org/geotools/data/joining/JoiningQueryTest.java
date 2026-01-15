3fb97c8cdf04065ec031097d41834344f5478d1f
Fernando Mino <fernandomino7@gmail.com>
2026-01-15 17:07:51 -0500

    Add support to inter-schema for app-schema
    joining support
    
    joining implementation
    
    fixes
    
    fixes
    
    fixes
    
    Initial connection test fixing not working
    
    connection test fix that worked
    
    fixes
    
    fixes
    
    fix
    
    Fix timeseries appschema tests
    
    Fix joining filtering

---
+++ modules/extension/app-schema/app-schema/src/test/java/org/geotools/data/joining/JoiningQueryTest.java
@@ -0,0 +1,66 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2026, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.data.joining;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+
+public class JoiningQueryTest {
+
+    @Test
+    public void testCopyConstructorDoesNotShareMutableState() {
+        JoiningQuery.QueryJoin join = new JoiningQuery.QueryJoin();
+        join.setJoiningTypeName("pozzi_decodificati_tr_pdf");
+        join.addId("id_temp");
+
+        JoiningQuery original = new JoiningQuery();
+        original.setQueryJoins(Collections.singletonList(join));
+        original.addId("root_id");
+
+        JoiningQuery copy = new JoiningQuery(original);
+        copy.getQueryJoins().get(0).addId("another_id");
+        copy.addId("other_root_id");
+
+        assertNotSame(original.getQueryJoins().get(0), copy.getQueryJoins().get(0));
+        assertEquals(1, original.getQueryJoins().get(0).getIds().size());
+        assertFalse(original.getIds().contains("other_root_id"));
+    }
+
+    @Test
+    public void testSetQueryJoinsCopiesInputJoins() {
+        JoiningQuery.QueryJoin externalJoin = new JoiningQuery.QueryJoin();
+        externalJoin.setJoiningTypeName("temp_connex");
+        externalJoin.addId("id_temp");
+        List<JoiningQuery.QueryJoin> joins = new ArrayList<>();
+        joins.add(externalJoin);
+
+        JoiningQuery query = new JoiningQuery();
+        query.setQueryJoins(joins);
+
+        externalJoin.addId("leaked_id");
+        joins.clear();
+
+        assertEquals(1, query.getQueryJoins().size());
+        assertEquals(1, query.getQueryJoins().get(0).getIds().size());
+    }
+}
